2251aed6705c367aa2f2414be974ed7b23059147
wannabefrontdeveloper <giwonk@naver.com>
2024-02-12 21:45:31 +0900

    코드수정 onCreate함수를 구현해 value 객체에 포함시켜주었습니다.

--- contexts/LogContext.js
+++ contexts/LogContext.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import {createContext, useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 const LogContext = createContext();
 
 export function LogContextProvider({children}) {
   const [logs, setLogs] = useState([]);
-  return <LogContext.Provider value={{logs}}>{children}</LogContext.Provider>;
+
+  const onCreate = ({title, body, date}) => {
+    const log = {
+      id: uuidv4(),
+      title,
+      body,
+      date,
+    };
+    setLogs([log, ...logs]);
+  };
+  return (
+    <LogContext.Provider value={{logs, onCreate}}>
+      {children}
+    </LogContext.Provider>
+  );
 }
 
 export default LogContext;
