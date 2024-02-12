b553be01e86ea2f4cd1b7834f0abb9d076030e1e
wannabefrontdeveloper <giwonk@naver.com>
2024-02-12 21:51:55 +0900

    코드수정 useContext를 사용해 LogContext의 값을 받아온 뒤, logs 배열을 콘솔에 출력하도록 하였습니다.

--- screens/FeedsScreen.js
+++ screens/FeedsScreen.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, View} from 'react-native';
 import FloatingWriteButton from '../components/FloatingWriteButton';
+import LogContext from '../contexts/LogContext';
 
 function FeedsScreen() {
+  const {logs} = useContext(LogContext);
+  console.log(JSON.stringify(logs, null, 2));
   return (
     <View style={styles.block}>
       <FloatingWriteButton />
