f9033c7313d276fd3cc11a2e0ba8a46516fec94c
wannabefrontdeveloper <giwonk@naver.com>
2024-02-12 23:06:07 +0900

    코드수정 간단한 애니메이션 연습코드입니다.

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useRef} from 'react';
+import {Animated, Button, StyleSheet, View} from 'react-native';
+
+function FadeInAndOut() {
+  const animation = useRef(new Animated.Value(1)).current;
+
+  return (
+    <View>
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            opacity: animation,
+          },
+        ]}
+      />
+      <Button
+        title="FadeIn"
+        onPress={() => {
+          Animated.timing(animation, {
+            toValue: 1,
+            useNativeDriver: true,
+          }).start();
+        }}
+      />
+      <Button
+        title="FadeOut"
+        onPress={() => {
+          Animated.timing(animation, {
+            toValue: 0,
+            useNativeDriver: true,
+          }).start();
+        }}
+      />
+    </View>
+  );
+}
 
 function CalendarScreen() {
-  return <View style={styles.block} />;
+  return (
+    <View style={styles.block}>
+      <FadeInAndOut />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {},
+  rectangle: {width: 100, height: 100, backgroundColor: 'black'},
 });
 
 export default CalendarScreen;
