ae48cf948fba7d53942cca62ac4a6bfc993a05e7
wannabefrontdeveloper <giwonk@naver.com>
2024-02-12 13:51:38 +0900

    코드추가 RootStack 컴포넌트를 사용하도록 하였습니다.

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import RootStack from './screens/RootStack';
 
 function App() {
-  return <NavigationContainer />;
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
 }
 
 export default App;
