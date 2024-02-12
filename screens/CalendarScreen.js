79b4928c341f45bb5791f86f141220b151e4349d
wannabefrontdeveloper <giwonk@naver.com>
2024-02-12 23:14:11 +0900

    코드 수정 컴포넌트가 좌우로 움직이는 효과를 구현하였습니다.

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,15 +1,16 @@
 import React, {useRef, useState, useEffect} from 'react';
 import {Animated, Button, StyleSheet, View} from 'react-native';
 
-function FadeInAndOut() {
-  const animation = useRef(new Animated.Value(1)).current;
-  const [hidden, setHidden] = useState(false);
+function SideLeftAndRight() {
+  const animation = useRef(new Animated.Value(0)).current;
+  const [enabled, setEnabled] = useState(false);
+
   useEffect(() => {
     Animated.timing(animation, {
-      toValue: hidden ? 0 : 1,
+      toValue: enabled ? 150 : 0,
       useNativeDriver: true,
     }).start();
-  }, [hidden, animation]);
+  }, [enabled, animation]);
 
   return (
     <View>
@@ -17,14 +18,14 @@ function FadeInAndOut() {
         style={[
           styles.rectangle,
           {
-            opacity: animation,
+            transform: [{translateX: animation}],
           },
         ]}
       />
       <Button
         title="Toggle"
         onPress={() => {
-          setHidden(!hidden);
+          setEnabled(!enabled);
         }}
       />
     </View>
@@ -34,7 +35,7 @@ function FadeInAndOut() {
 function CalendarScreen() {
   return (
     <View style={styles.block}>
-      <FadeInAndOut />
+      <SideLeftAndRight />
     </View>
   );
 }
