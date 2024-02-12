b02a50eaa6dfdc14af69cbb0c94c08424e5e9dd3
wannabefrontdeveloper <giwonk@naver.com>
2024-02-12 16:02:02 +0900

    코드수정 WriteEditor를 사용하도록 하였습니다.

--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import WriteEditor from '../components/WriteEditor';
 import WriteHeader from '../components/WriteHeader';
 
 function WriteScreen() {
   return (
     <SafeAreaView style={styles.block}>
       <WriteHeader />
+      <WriteEditor />
     </SafeAreaView>
   );
 }
