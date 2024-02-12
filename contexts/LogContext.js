d343f9a6141d5e56c209ff2fc5f8da6ce7af0eee
wannabefrontdeveloper <giwonk@naver.com>
2024-02-12 21:37:15 +0900

    코드수정 기존에 만든 text 상태는 지우고 logs라는 배열을 useState로 상태관리하도록 하였습니다.

--- contexts/LogContext.js
+++ contexts/LogContext.js
@@ -4,12 +4,8 @@ import {createContext, useState} from 'react';
 const LogContext = createContext();
 
 export function LogContextProvider({children}) {
-  const [text, setText] = useState('');
-  return (
-    <LogContext.Provider value={{text, setText}}>
-      {children}
-    </LogContext.Provider>
-  );
+  const [logs, setLogs] = useState([]);
+  return <LogContext.Provider value={{logs}}>{children}</LogContext.Provider>;
 }
 
 export default LogContext;
