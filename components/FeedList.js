60562cb249e33c551fc464c7b5c24365c48ae83c
wannabefrontdeveloper <giwonk@naver.com>
2024-02-12 23:23:16 +0900

    코드수정 스크롤이 바닥에 가까워졌는지 감지하는 기능을 구현하였습니다.

--- components/FeedList.js
+++ components/FeedList.js
@@ -3,6 +3,17 @@ import {FlatList, StyleSheet, View} from 'react-native';
 import FeedListItem from './FeedListItem';
 
 function FeedList({logs}) {
+  const onScroll = e => {
+    const {contentSize, layoutMeasurement, contentOffset} = e.nativeEvent;
+    const distanceFromBottom =
+      contentSize.height - layoutMeasurement.height - contentOffset.y;
+
+    if (distanceFromBottom < 72) {
+      console.log('바닥과 가까워요');
+    } else {
+      console.log('바닥과 멀어졌어요.');
+    }
+  };
   return (
     <FlatList
       data={logs}
@@ -10,6 +21,7 @@ function FeedList({logs}) {
       renderItem={({item}) => <FeedListItem log={item} />}
       keyExtractor={log => log.id}
       ItemSeparatorComponent={() => <View style={styles.separator} />}
+      onScroll={onScroll}
     />
   );
 }
