23f3d566cfd3a35a7e689d5eabb734dd5c67ddb9
wannabefrontdeveloper <giwonk@naver.com>
2024-02-12 14:31:11 +0900

    코드수정 탭에 아이콘을 설정하고, 화면에 타이틀을 설정했으며, 탭의 스타일도 변경해주었습니다.

--- screens/MainTab.js
+++ screens/MainTab.js
@@ -3,15 +3,44 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import FeedsScreen from './FeedsScreen';
 import CalendarScreen from './CalendarScreen';
 import SearchScreen from './SearchScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
 function MainTab() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Feeds" component={FeedsScreen} />
-      <Tab.Screen name="Calendar" component={CalendarScreen} />
-      <Tab.Screen name="Serach" component={SearchScreen} />
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        activeTinitColor: '#009688',
+      }}>
+      <Tab.Screen
+        name="Feeds"
+        component={FeedsScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="view-stream" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={CalendarScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="event" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
