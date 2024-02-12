f908580e24663a2b6fce2436e08ce2308a5a450a
wannabefrontdeveloper <giwonk@naver.com>
2024-02-12 22:05:55 +0900

    코드생성 log 객체를 Props로 받아와서 알맞은 위치에 정보를 보여주는 컴포넌트입니다.

---
+++ components/FeedListItem.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Platform, Pressable, StyleSheet, Text} from 'react-native';
+
+function truncate(text) {
+  // 정규식을 사용해 모든 줄 바꿈 문자 제거
+  const replaced = text.replace(/\n/g, ' ');
+  if (replaced.length <= 100) {
+    return replaced;
+  }
+  return replaced.slice(0, 100).concat('...');
+}
+
+function FeedListItem({log}) {
+  const {title, body, date} = log; // 사용하기 편하게 객체 구조 분해 할당
+
+  return (
+    <Pressable
+      style={({pressed}) => [
+        styles.block,
+        Platform.OS === 'ios' && pressed && {backgroundColor: '#efefef'},
+      ]}
+      android_ripple={{color: '#ededed'}}>
+      <Text style={styles.date}>{new Date(date).toLocaleDateString()}</Text>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.body}>{truncate(body)}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    backgroundColor: 'white',
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+  },
+  date: {
+    fontSize: 12,
+    color: '#546e7a',
+    marginBottom: 8,
+  },
+  title: {
+    color: '#263238',
+    marginbottom: 8,
+  },
+  body: {
+    color: '#37474f',
+    fontSize: 16,
+    lineHeight: 21,
+  },
+});
+
+export default FeedListItem;
