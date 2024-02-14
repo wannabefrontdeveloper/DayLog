f63f6b30235940997756367596e7dc19b00114af
wannabefrontdeveloper <giwonk@naver.com>
2024-02-14 15:46:26 +0900

    코드수정 CalendarView 컴포넌트를 사용하도록 하였습니다.

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,59 +1,8 @@
-import React, {useRef, useState, useEffect} from 'react';
-import {Animated, Button, StyleSheet, View} from 'react-native';
-
-function SideLeftAndRight() {
-  const animation = useRef(new Animated.Value(0)).current;
-  const [enabled, setEnabled] = useState(false);
-
-  useEffect(() => {
-    Animated.timing(animation, {
-      toValue: enabled ? 1 : 0,
-      useNativeDriver: true,
-    }).start();
-  }, [enabled, animation]);
-
-  return (
-    <View>
-      <Animated.View
-        style={[
-          styles.rectangle,
-          {
-            transform: [
-              {
-                translateX: animation.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [0, 150],
-                }),
-              },
-            ],
-            opacity: animation.interpolate({
-              inputRange: [0, 1],
-              outputRange: [1, 0],
-            }),
-          },
-        ]}
-      />
-      <Button
-        title="Toggle"
-        onPress={() => {
-          setEnabled(!enabled);
-        }}
-      />
-    </View>
-  );
-}
+import React from 'react';
+import CalendarView from '../components/CalendarView';
 
 function CalendarScreen() {
-  return (
-    <View style={styles.block}>
-      <SideLeftAndRight />
-    </View>
-  );
+  return <CalendarView />;
 }
-
-const styles = StyleSheet.create({
-  block: {},
-  rectangle: {width: 100, height: 100, backgroundColor: 'black'},
-});
 
 export default CalendarScreen;
