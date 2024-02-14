c300febc74fc08c1a1956c66767518017231c091
wannabefrontdeveloper <giwonk@naver.com>
2024-02-14 16:02:39 +0900

    코드수정 selectedDate라는 상태를 만들어주었습니다.

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,16 +1,25 @@
 import {format} from 'date-fns';
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import CalendarView from '../components/CalendarView';
 import LogContext from '../contexts/LogContext';
 
 function CalendarScreen() {
   const {logs} = useContext(LogContext);
+  const [selectedDate, setSelectedDate] = useState(
+    format(new Date(), 'yyyy-MM-dd'),
+  );
   const markedDates = logs.reduce((acc, current) => {
     const formattedDate = format(new Date(current.date), 'yyyy-MM-dd');
     acc[formattedDate] = {marked: true};
     return acc;
   }, {});
-  return <CalendarView markedDates={markedDates} />;
+  return (
+    <CalendarView
+      markedDates={markedDates}
+      selectedDate={selectedDate}
+      onSelectedDate={setSelectedDate}
+    />
+  );
 }
 
 export default CalendarScreen;
