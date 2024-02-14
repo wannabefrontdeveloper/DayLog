a8dcdaf05b24f156e3936d0152fd278ac8a6eb86
wannabefrontdeveloper <giwonk@naver.com>
2024-02-14 14:50:54 +0900

    코드수정 수정 기능을 구현하였습니다.

--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -13,14 +13,23 @@ function WriteScreen({route}) {
   const [body, setBody] = useState(log?.body ?? '');
   const navigation = useNavigation();
 
-  const {onCreate} = useContext(LogContext);
+  const {onCreate, onModify} = useContext(LogContext);
   const onSave = () => {
-    onCreate({
-      title,
-      body,
-      // 날짜를 문자열로 변환
-      date: new Date().toISOString(),
-    });
+    if (log) {
+      onModify({
+        id: log.id,
+        date: log.date,
+        title,
+        body,
+      });
+    } else {
+      onCreate({
+        title,
+        body,
+        // 날짜를 문자열로 변환
+        date: new Date().toISOString(),
+      });
+    }
     navigation.pop();
   };
   return (
