47d51a010f06602fd9943abd6e1cd40db0ebce30
wannabefrontdeveloper <giwonk@naver.com>
2024-02-14 15:55:56 +0900

    코드수정 CalendarScreen에서 LogContext의 logs 배열을 받아오도록 하였습니다.

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import CalendarView from '../components/CalendarView';
+import LogContext from '../contexts/LogContext';
 
 function CalendarScreen() {
+  const {logs} = useContext(LogContext);
   return <CalendarView />;
 }
 
