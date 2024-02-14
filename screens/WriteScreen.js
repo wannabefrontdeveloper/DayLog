54799e1791fa04758788776e676b8b30c1e450c7
wannabefrontdeveloper <giwonk@naver.com>
2024-02-14 15:00:02 +0900

    코드수정 onAskRemove라는 함수를 만들어서 WriteHeader에 Props로 전달해주었습니다.

--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -1,6 +1,6 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useContext, useState} from 'react';
-import {KeyboardAvoidingView, Platform, StyleSheet} from 'react-native';
+import {Alert, KeyboardAvoidingView, Platform, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import WriteEditor from '../components/WriteEditor';
 import WriteHeader from '../components/WriteHeader';
@@ -13,7 +13,7 @@ function WriteScreen({route}) {
   const [body, setBody] = useState(log?.body ?? '');
   const navigation = useNavigation();
 
-  const {onCreate, onModify} = useContext(LogContext);
+  const {onCreate, onModify, onRemove} = useContext(LogContext);
   const onSave = () => {
     if (log) {
       onModify({
@@ -32,12 +32,36 @@ function WriteScreen({route}) {
     }
     navigation.pop();
   };
+  const onAskRemove = () => {
+    Alert.alert(
+      '삭제',
+      '정말로 삭제하시겠어요?',
+      [
+        {text: '취소', style: 'cancel'},
+        {
+          text: '삭제',
+          onPress: () => {
+            onRemove(log?.id);
+            navigation.pop();
+          },
+          style: 'destructive',
+        },
+      ],
+      {
+        cancelable: true,
+      },
+    );
+  };
   return (
     <SafeAreaView style={styles.block}>
       <KeyboardAvoidingView
         style={styles.avoidingView}
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-        <WriteHeader onSave={onSave} />
+        <WriteHeader
+          onSave={onSave}
+          onAskRemove={onAskRemove}
+          isEditing={!!log}
+        />
         <WriteEditor
           title={title}
           body={body}
