7f0489a1e7600be749e64b991c0921db58391c08
wannabefrontdeveloper <giwonk@naver.com>
2024-02-14 16:00:48 +0900

    코드수정 logs 배열을 Calendar 컴포넌트의 markedDates Props 형태로 변환해주고 CalendarView에 전달해주도록 하였습니다.

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,10 +1,16 @@
+import {format} from 'date-fns';
 import React, {useContext} from 'react';
 import CalendarView from '../components/CalendarView';
 import LogContext from '../contexts/LogContext';
 
 function CalendarScreen() {
   const {logs} = useContext(LogContext);
-  return <CalendarView />;
+  const markedDates = logs.reduce((acc, current) => {
+    const formattedDate = format(new Date(current.date), 'yyyy-MM-dd');
+    acc[formattedDate] = {marked: true};
+    return acc;
+  }, {});
+  return <CalendarView markedDates={markedDates} />;
 }
 
 export default CalendarScreen;
