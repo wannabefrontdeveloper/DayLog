2f15275ad326045d6f24bd6ccdbecfd830bd27b2
wannabefrontdeveloper <giwonk@naver.com>
2024-02-14 15:20:32 +0900

    코드추가 검색어를 입력할 TextInput과 검색어를 초기화하는 버튼을 구현하였습니다.

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import {StyleSheet, useWindowDimensions, View} from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  TextInput,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function SearchHeader() {
   const {width} = useWindowDimensions();
-  return <View style={[styles.block, {width: width - 32, height: 24}]} />;
+  return (
+    <View style={[styles.block, {width: width - 32}]}>
+      <TextInput
+        style={styles.input}
+        placeholder="검색어를 입력하세요"
+        autoFocus
+      />
+      <Pressable
+        style={({pressed}) => [styles.button, pressed && {opacity: 0.5}]}>
+        <Icon name="cancel" size={20} color="#9e9e9e" />
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
-    color: 'white',
-    backgroundColor: 'blue',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+  },
+  button: {
+    marginLeft: 8,
   },
 });
 
